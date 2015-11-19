2d69db09820ecf483e8b33f4cfb8a74aa20b5be8
Alien Fernandez <alien@kruger.com.ec>
2015-11-19 11:21:13 -0500

    Actualizando index.html reestructurando freamework

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import 'jquery';
 import angular from 'angular';
-import 'angular-material';
+/**
+ * Import theme
+ */
+import 'font-awesome';
 import 'bootstrap';
+
 import 'angular-ui-router';
 import 'ocLazyLoad';
-import 'ngStorage';
-import 'angular-local-storage';
 import 'ui-router-stateHelper';
 
 //Importanto recursos comunes
@@ -14,10 +16,8 @@ import futureRoutes from './routes.json!';
 
 let app = angular.module('app', ['ui.router',
     'ui.router.stateHelper',
-    'oc.lazyLoad',
-    'ngMaterial',
-    'ngStorage',
-    'LocalStorageModule']);
+    'oc.lazyLoad'
+]);
 
 /*Config Theme*/
 app.config(() => {
