ba77d9689b08237deb4600df4cd621e0a0554561
Alien Fernandez <alien@kruger.com.ec>
2016-01-13 16:47:05 -0500

    Actualizando log params en dev env

--- config/env/development.js
+++ config/env/development.js
@@ -17,12 +17,22 @@ module.exports = {
         "port": 6379
     },
     log: {
+        // logging with Morgan - https://github.com/expressjs/morgan
         // Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
         format: 'dev',
-        // Stream defaults to process.stdout
-        // Uncomment to enable logging to a log on the file system
         options: {
-            stream: 'access.log'
+            // Stream defaults to process.stdout
+            // Uncomment/comment to toggle the logging to a log on the file system
+            //stream: {
+            //  directoryPath: process.cwd(),
+            //  fileName: 'access.log',
+            //  rotatingLogs: { // for more info on rotating logs - https://github.com/holidayextras/file-stream-rotator#usage
+            //    active: false, // activate to use rotating logs
+            //    fileName: 'access-%DATE%.log', // if rotating logs are active, this fileName setting will be used
+            //    frequency: 'daily',
+            //    verbose: false
+            //  }
+            //}
         }
     },
     app: {
